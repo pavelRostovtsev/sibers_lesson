ad1612456f6f65e9da93bc44d907733ac1ce077b
Pavel Rostovtsev <zavet199912@gmail.com>
2020-10-29 14:45:09 +0600

    swap_file

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,6 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
@@ -14,16 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $a = 10;
-        $array = ['Hi', 12,0, 444];
-        $bool_true = true;
-        $bool_false = false;
-        return $this->render('default/index.html.twig', [
-            'a' => $a,
-            'array' => $array,
-            'bool_true' => $bool_true,
-            'bool_false' => $bool_false,
-        ]);
+        return $this->render('@App/default/index.html.twig');
     }
 
     /**
@@ -32,7 +22,7 @@ class DefaultController extends Controller
     public function feedbackAction()
     {
 
-        return $this->render('default/feedback.html.twig');
+        return $this->render('@App/default/feedback.html.twig');
     }
 
 }
