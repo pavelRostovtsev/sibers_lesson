60bb6be9eb4c1fb9c6422bd224352f91ce34a7c0
Pavel Rostovtsev <zavet199912@gmail.com>
2020-10-29 12:49:41 +0600

    'testing twig'

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,8 +15,15 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         $a = 10;
-        return $this->render('default/index.html.twig',
-        ['a' => $a]);
+        $array = ['Hi', 12,0, 444];
+        $bool_true = true;
+        $bool_false = false;
+        return $this->render('default/index.html.twig', [
+            'a' => $a,
+            'array' => $array,
+            'bool_true' => $bool_true,
+            'bool_false' => $bool_false,
+        ]);
     }
 
     /**
